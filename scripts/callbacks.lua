7dc0af1848f0b284595bc0a270da433f574af921
HeyJOe <jocarmi03@gmail.com>
2022-10-01 06:38:39 +0200

    first commit

---
+++ scripts/callbacks.lua
@@ -0,0 +1,27 @@
+
+
+
+local turno = true
+
+
+
+--- @param gre#context mapargs
+function cell_pressed(mapargs)
+  print(mapargs.row)
+  print(mapargs.col)
+  
+  -- change image 
+  
+  -- check if someone has won
+  
+  turno = not(turno)
+  
+  -- computer move
+  
+  -- computer cell set
+  
+  -- check if someone has won
+  
+  turno = not(turno)
+  
+end
